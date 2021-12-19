5b98ca88741fd8cd981d7b5792659fc532d8c1b7
qiushao <qiushaocloud@126.com>
2021-12-19 18:41:22 +0800

    add push-remote-git-repos.sh

--- push-remote-git-repos.sh
+++ push-remote-git-repos.sh
@@ -1,21 +1,16 @@
 CURR_DIR=$(cd "$(dirname "$0")"; pwd)
 
-# 全部替换为 GIT_REPLACE_URL
-sed -i 's/ssh:\/\/git@www.qiushaocloud.top:61123\//GIT_REPLACE_URL/g' $CURR_DIR/.git/config
-sed -i 's/git@gitee.com:/GIT_REPLACE_URL/g' $CURR_DIR/.git/config
-sed -i 's/git@github.com:/GIT_REPLACE_URL/g' $CURR_DIR/.git/config
+gitName=`grep -E "url = .*.git" $CURR_DIR/.git/config|awk -F 'qiushaocloud/' '{print $2}'`
 
 # 推送到自建的 gitlab
-sed -i 's/GIT_REPLACE_URL/ssh:\/\/git@www.qiushaocloud.top:61123\//g' $CURR_DIR/.git/config
+git remote set-url origin ssh://git@www.qiushaocloud.top:61123/qiushaocloud/$gitName
 git push
-sed -i 's/ssh:\/\/git@www.qiushaocloud.top:61123\//GIT_REPLACE_URL/g' $CURR_DIR/.git/config
 
 # 推送到 gitee
-sed -i 's/GIT_REPLACE_URL/git@gitee.com:/g' $CURR_DIR/.git/config
+git remote set-url origin git@gitee.com:qiushaocloud/$gitName
 git push
-sed -i 's/git@gitee.com:/GIT_REPLACE_URL/g' $CURR_DIR/.git/config
 
 # 推送到 github
-sed -i 's/GIT_REPLACE_URL/git@github.com:/g' $CURR_DIR/.git/config
+git remote set-url origin git@github.com:qiushaocloud/$gitName
 git push
 
