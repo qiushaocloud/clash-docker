43c49ebb7663d9109301a351d9f094da466324ac
邱少羽梦 <qiushaocloud@126.com>
2023-06-19 23:27:22 +0800

    Update push-remote-git-repos.sh

--- push-remote-git-repos.sh
+++ push-remote-git-repos.sh
@@ -16,22 +16,22 @@ echo "gitName: $gitName"
 # 推送到 gitee
 echo "推送到 gitee: git@gitee.com:qiushaocloud/$gitName"
 git remote set-url origin git@gitee.com:qiushaocloud/$gitName
-git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push
 
 # 推送到 github
 echo "推送到 github: git@github.com:qiushaocloud/$gitName"
 git remote set-url origin git@github.com:qiushaocloud/$gitName
-git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push
 
 # 推送到 gitlab
 echo "推送到 gitlab: git@gitlab.com:qiushaoyumeng/$gitName"
 git remote set-url origin git@gitlab.com:qiushaoyumeng/$gitName
-git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push
 
 # 推送到 gitcode
 echo "推送到 gitcode: git@gitcode.net:qiushaocloud/$gitName"
 git remote set-url origin git@gitcode.net:qiushaocloud/$gitName
-git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push
 
 # 推送到自建的 gitea
 # echo "推送到自建的 gitea: ssh://git@gitea.qiushaocloud.top:61322/qiushaocloud/$gitName"
@@ -41,7 +41,7 @@ git push
 # 推送到自建的 gitlab
 echo "推送到自建的 gitlab: ssh://git@gitlab.qiushaocloud.top:61023/qiushaocloud/$gitName"
 git remote set-url origin ssh://git@gitlab.qiushaocloud.top:61023/qiushaocloud/$gitName
-git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push
 
 echo "mv config"
 mv $CURR_DIR/.git/config.bak $CURR_DIR/.git/config
